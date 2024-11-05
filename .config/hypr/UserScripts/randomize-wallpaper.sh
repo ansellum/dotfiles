ea9c2d754d7040f082dccc305467d0def0c59122
ansellum <jacobvogt27@gmail.com>
2024-11-05 14:14:29 -0800

    added hyprland configs

---
+++ .config/hypr/UserScripts/randomize-wallpaper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Script for Random Wallpaper ( CTRL ALT W)
+
+wallDIR="$HOME/Pictures/wallpapers"
+
+focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
+
+PICS=($(find ${wallDIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+
+# Transition config
+FPS=144
+TYPE="random"
+DURATION=1
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
+
+
+swww query || swww-daemon --format xrgb && swww img -o $focused_monitor ${RANDOMPICS} $SWWW_PARAMS
